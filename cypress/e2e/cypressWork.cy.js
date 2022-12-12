2145e6f8b540d317c5733b9907ff847022017401
Nandish-P <72277827+Nandu890@users.noreply.github.com>
2022-12-12 20:24:33 +0530

    7

--- cypress/e2e/cypressWork.cy.js
+++ cypress/e2e/cypressWork.cy.js
@@ -6,4 +6,5 @@ it("Login to application",()=>{
   it("proceed application",()=>{
       cy.visit("https://docs.cypress.io/guides/tooling/reporters#Installed-via-npm");
       cy.get('li a').contains("Merging reports across spec files");
+      cy.log("This project is updated");;
     })
